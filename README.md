b50d633334660b5ec5b82baefe2dcc3aa9629632
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-07-10 00:14:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Spigot Kotlin Template
+
+This repository is a template for creating Spigot plugin in Kotlin.
+
+It was created for my own use, so there may be many parts that are difficult to use.
+
+If you find it difficult to use, please submit a PR or Issue.
+
+## Usage
+
+### 1. Change package name.
+
+Change the package name in the following directory:
+
+``/src/main/kotlin/``
+
+> **Note**
+> Default: ``io.github.rokuosan.template``
+
+### 2. Edit ``gradle.properties``
+
+Rewrite the value in gradle.properties to the value of the plugin you are creating.
+
+| Property Name | Setting Value |
+| :-: | :-: |
+| mainClassName | Use in Application Block |
+| group | Your domain |
+| name | Plugin Name |
+| version | Plugin version |
+| author | Your name |
+| apiVersion | Require Spigot API version |
+| prefix | Plugin's prefix |
+| javaVersion | Target JVM version |
+| minecraftVersion | Target MC version |
+
+
+### 3. Just build!
+
+Use following command:
+
+```shell
+./gradlew shadowJar
+```
+
+The results are outputs to ``/build/libs``.
+
+## Default versions
+
+| Name | Version |
+| :-: | :-: |
+| Java | 17 |
+| Kotlin | 1.6.21 |
+| Minecraft | 1.18.2 |
+| Package | 0.1.0 |
+| ShadowJar | 7.1.2 |
+
---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+// Package Information
+// -> Please edit gradle.properties
+val javaVersion = findProperty("kotlinVersion")?:"17"
+val mainClassPath: String by project
+val minecraftVersion: String by project
+val name: String by project
+val author: String by project
+val apiVersion: String by project
+val prefix: String by project
+group = findProperty("group").toString()
+version = findProperty("version").toString()
+java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
+
+
+// Plugins
+plugins {
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
+    id ("com.github.johnrengelman.shadow") version "7.1.2"
+    application
+}
+
+
+// Repositories
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+
+// Dependencies
+dependencies {
+    // Kotlin Serialization (For Parsing JSON)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+
+    // Minecraft Spigot API
+    compileOnly("org.spigotmc:spigot-api:${minecraftVersion}-R0.1-SNAPSHOT")
+
+    // Kotlin Test Library
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+// Configurations
+application {
+    mainClass.set(mainClassPath)
+}
+tasks.withType<KotlinCompile>{
+    kotlinOptions{
+        jvmTarget = "17"
+    }
+}
+@Suppress("UnstableApiUsage")
+tasks.withType<ProcessResources>{
+    mapOf(
+        "version" to version,
+        "mainClass" to mainClassPath,
+        "name" to name,
+        "author" to author,
+        "apiVersion" to apiVersion,
+        "prefix" to prefix
+    ).let{ props ->
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml"){
+            expand(props)
+        }
+    }
+}
